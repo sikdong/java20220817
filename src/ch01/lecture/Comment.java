650ce1f46cad84c3c488eec5d50b0ed3ff452eda
sikdong <zmstnv1119@naver.com>
2022-08-17 12:13:36 +0900

    0817

---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,22 @@
+package ch01.lecture;
+
+public class Comment {
+	public static void main(String[] args) {
+		// 주석 : 프로그램 실행에 영향을 미치지 않음 
+		// 한줄 주석
+		
+		/*
+		 * 여러줄 주석
+		 */
+		
+		// 힌트 얻기 : ctrl + space
+		// 한줄 지우기 : ctrl + d
+		// 코드 위아래로 옮기기 : alt + 방향키
+
+		// 콘솔 출력 코드 : sysout / sout + ctrl + space
+		System.out.println("Hello world!");
+		System.out.println("자바는 쉽지 않다");
+		System.out.println("잘 배워서 써먹어 보자");
+	}
+
+}
