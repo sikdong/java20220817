78d90e066b0e61fe8734de148cec3d7b11783929
sikdong <zmstnv1119@naver.com>
2022-08-19 18:27:04 +0900

    0819(풀어보기)

--- src/ch05/Exercises/Exercise08.java
+++ src/ch05/Exercises/Exercise08.java
@@ -7,18 +7,17 @@ public class Exercise08 {
 			{83, 92, 96},
 			{78, 83, 93, 87, 88}
 		};
-			
+		
 		int sum = 0;
 		double avg = 0.0;
 		
-		for (int i = 0; i < array.length; i++) {
-				for(int j = 0; j < array[O].length; j++) {
-					for(int a = 0; a < array[1].length; a++) {
-						for(int b = 0; b < array[2].length; b++) {
+		for (int i = 0; i < 3; i++) {
+				for(int j = 0; j < 5; j++) {
 					sum += array[i][j];
 				}
 			}
 		 System.out.println("sum: " + sum);
 		}
 	}
+	
 
