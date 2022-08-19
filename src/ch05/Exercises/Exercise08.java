a547759ae51badaf7c68508240fd1b88f9dc6011
sikdong <zmstnv1119@naver.com>
2022-08-19 18:18:26 +0900

    0819(자습)

---
+++ src/ch05/Exercises/Exercise08.java
@@ -0,0 +1,24 @@
+package ch05.Exercises;
+
+public class Exercise08 {
+	public static void main(String[] args) {
+		int[][] array = {
+			{95, 86},
+			{83, 92, 96},
+			{78, 83, 93, 87, 88}
+		};
+			
+		int sum = 0;
+		double avg = 0.0;
+		
+		for (int i = 0; i < array.length; i++) {
+				for(int j = 0; j < array[O].length; j++) {
+					for(int a = 0; a < array[1].length; a++) {
+						for(int b = 0; b < array[2].length; b++) {
+					sum += array[i][j];
+				}
+			}
+		 System.out.println("sum: " + sum);
+		}
+	}
+
