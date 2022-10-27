27b3cbb2010a41eee31c8a01afa490bd5566d47c
sikdong <zmstnv1119@naver.com>
2022-10-27 17:46:32 +0900

    while문 추가

--- src/Programmers/giveUpMath.java
+++ src/Programmers/giveUpMath.java
@@ -2,29 +2,59 @@ package Programmers;
 
 public class giveUpMath {
 	public static void main(String[] args) {
-		int[] num1 = {1,2,3,4,5};
-		int[] num2 = {2,1,2,3,2,4,2,5};
-		int[] num3 = {3,3,1,1,2,2,4,4,5,5};
+		int[] answer = {1,3,2,4,2};
+		int[] num1 = { 1, 2, 3, 4, 5 };
+		int[] num2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+		int[] num3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+		boolean go = true;
+		int check1 = 0;
+		int check2 = 0;
+		int check3 = 0;
+		int j = 0;
 		
-		
-	}
-	
-	public int searchMyself(int[] num, int[] n) {
-		int solution = 0;
-		int cnt = 0;
-		for(int i = 0; i < num.length; i++) {
-			cnt++;
-			if(num[i]==n[i]) {
-				solution++;
+		while (go) {
+			for (int i = 0; i < num1.length; i++) {
+				if (num1[i] == answer[j]) {
+					
+					check1++;
+				}
+				j++;
+			}
+			if (j == answer.length) {
+				go = false;
 			}
 		}
-		if(cnt != n.length) {
-			searchMyself(num, n);
-		} else {
-			
-			return solution;
+		System.out.println(j);
+		System.out.println(check1);
+		j = 0;
+		while (go) {
+			for (int i = 0; i < num2.length; i++) {
+				if (num2[i] == answer[j]) {
+					
+					check2++;
+				}
+				j++;
+			}
+			if (j == answer.length) {
+				go = false;
+			}
 		}
+		System.out.println(check2);
+		j=0;
+		while (go) {
+			for (int i = 0; i < num3.length; i++) {
+				if (num3[i] == answer[j]) {
+					
+					check3++;
+				}
+				j++;
+			}
+			if (j == answer.length) {
+				go = false;
+			}
+		}
+		System.out.println(check3);
+		
 		
 	}
-	
 }
